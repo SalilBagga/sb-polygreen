77f4043a6654139896d296a223ab1bafa2c9ee4d
salilsuraasa <salil@suraasa.com>
2024-04-05 00:11:43 +0530

    feat: setup shadcn

--- src/features/TopNavigation/index.tsx
+++ src/features/TopNavigation/index.tsx
@@ -1,14 +1,59 @@
 import React from 'react';
 import Logo from '../../assets/Logo.webp';
+import { Button } from '../../@/components/ui/button';
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuItem,
+} from '@/@/components/ui/dropdown-menu';
+import { ChevronDown } from 'lucide-react';
 
 const TopNavigation = () => {
   return (
-    <nav className="py-4 px-12 bg-white flex items-center">
+    <nav className="py-4 px-12 bg-white flex justify-between items-center">
       <div className="flex items-center">
         <img src={Logo} alt="logo" className="w-[91.43px] h-16 mr-6" />
-        <h1>Sali Balaji Polygreen Industries</h1>
+        <h1 className="text-[32px] font-medium leading-[37.57px] text-left">
+          Sali Balaji Polygreen Industries
+        </h1>
       </div>
-      <div></div>
+      <div className="gap-5">
+        <Button variant="ghost">Home</Button>
+        <DropdownMenu>
+          <DropdownMenuTrigger>
+            <Button variant="ghost">
+              Products
+              <ChevronDown className="w-4 h-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuItem>Item 1</DropdownMenuItem>
+            <DropdownMenuItem>Item 2</DropdownMenuItem>
+            <DropdownMenuItem>Item 3</DropdownMenuItem>
+            <DropdownMenuItem>Item 4</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+        <Button variant="ghost">Infrastructure</Button>
+        <Button variant="ghost">About Us</Button>
+        <DropdownMenu>
+          <DropdownMenuTrigger>
+            <Button variant="ghost">
+              Gallery
+              <ChevronDown className="w-4 h-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuItem>Item 1</DropdownMenuItem>
+            <DropdownMenuItem>Item 2</DropdownMenuItem>
+            <DropdownMenuItem>Item 3</DropdownMenuItem>
+            <DropdownMenuItem>Item 4</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+        <Button className="w-[152px] h-[67px] rounded-none">Inquire now</Button>
+      </div>
     </nav>
   );
 };
