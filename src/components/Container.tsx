b58596fc1bb40dcc2f8b4e79f099637ccaa87812
salilsuraasa <salil@suraasa.com>
2024-04-05 01:57:52 +0530

    feat: setup container

---
+++ src/components/Container.tsx
@@ -0,0 +1,20 @@
+import clsx from 'clsx';
+import React, { PropsWithChildren } from 'react';
+
+const Container = ({
+  children,
+  fullWidth,
+  className,
+}: PropsWithChildren<{ fullWidth?: boolean; className?: string }>) => {
+  return (
+    <div
+      className={clsx('w-full mx-auto', className, {
+        'max-w-[1280px] md:px-40 sm:px-10 px-4': !fullWidth,
+      })}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default Container;
